98dccf65a6d6810eb71bfd7d5eab67844a17b82d
root <root@zhangyz.(none)>
2017-07-26 05:30:46 +0800

    commit

--- BeatALL/BeatALL/settings.py
+++ BeatALL/BeatALL/settings.py
@@ -78,10 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'beatall',
-        'HOST': '',
-        'USER': '',
-        'PASSWORD': '',
-        'PORT': ,
+        'HOST': '127.0.0.1',
+        'USER': 'beatall',
+        'PASSWORD': '123456',
+        'PORT': 3306,
     }
 }
 
@@ -108,18 +108,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
+
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
