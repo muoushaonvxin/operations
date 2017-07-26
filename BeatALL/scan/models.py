10976ce97f9015bbbd083193e45491e69510dee3
root <root@zhangyz.(none)>
2017-07-26 23:29:58 +0800

    commit

--- BeatALL/scan/models.py
+++ BeatALL/scan/models.py
@@ -3,4 +3,9 @@ from django.db import models
 # Create your models here.
 class HostScan(models.Model):
     host = models.IPAddressField(max_length=32,verbose_name=u"主机地址")
-    port = models.IntegerField(max_length=10, verbose_name=u"端口号")
+    port = models.IntegerField(verbose_name=u"端口号")
+    add_time = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.host
+
