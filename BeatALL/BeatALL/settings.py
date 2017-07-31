d14f83a0b4a60315ee258e569f5b00c9a9c56753
zhangyz <zhangyuzeoffice@163.com>
2017-07-31 08:36:22 -0400

    commit

--- BeatALL/BeatALL/settings.py
+++ BeatALL/BeatALL/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'attack',
     'spider',
     'scan',
+    'passcracking',
 ]
 
 MIDDLEWARE = [
--- BeatALL/passcracking/models.py
+++ BeatALL/passcracking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class PasswordCrack(models.Model):
+    pass
+
+
+class ssh_crack(models.Model):
+    host = models.GenericIPAddressField(verbose_name=u"目标ip地址")
+    name = models.CharField(max_length=32, null=True, blank=True, verbose_name=u"目标命名")
+
+
+class ssh_crack_detail(models.Model):
+    password = models.CharField(max_length=50, verbose_name=u"破解的密码")
+    ssh_crack = models.ForeignKey(ssh_crack)
+
+    def __str__(self):
+        return "<%s>" % self.password
