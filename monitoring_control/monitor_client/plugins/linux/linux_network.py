f899ca47510af0b132526f1d9cfd91aa06d3ab1a
root <root@zhangyz.(none)>
2017-09-07 05:41:02 +0800

    commit

--- monitoring_control/monitor_client/plugins/linux/linux_network.py
+++ monitoring_control/monitor_client/plugins/linux/linux_network.py
@@ -3,13 +3,17 @@
 import subprocess
 
 def monitor():
-	shell_command = 'sar -n DEV 1 5 | grep -v IFACE | grep Average > ./network.txt'
-	result = subprocess.Popen(shell_command,shell=True,stdout=subprocess.PIPE)
-	value_dict = {'status': 0, 'data': {}}
+	try:
+		shell_command = 'sar -n DEV 1 5 | grep -v IFACE | grep Average > ./network.txt'
+		result = subprocess.Popen(shell_command,shell=True,stdout=subprocess.PIPE)
+		value_dict = {'status': 0, 'data': {}}
 
-	files = open('./network.txt', 'r').readlines()
-	for line in files:
-		line = line.split()
-		nic_name, t_in, t_out = line[1], line[4], line[5]
-		value_dict['data'][nic_name] = {"t_in": line[4], "t_out": line[5]}
+		files = open('./network.txt', 'r').readlines()
+		for line in files:
+			line = line.split()
+			nic_name, t_in, t_out = line[1], line[4], line[5]
+			value_dict['data'][nic_name] = {"t_in": line[4], "t_out": line[5]}
+	except Exception as e:
+		value_dict = {'status': 250, 'data': {}}
+		return value_dict
 	return value_dict
