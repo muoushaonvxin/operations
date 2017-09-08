9b2a6f081a5e79132efd1624d015c0cf522d05a7
zhangyz <zhangyuzeoffice@163.com>
2017-09-09 04:30:07 +0800

    commit

--- monitoring_control/monitor_client/plugins/linux/linux_cpu.py
+++ monitoring_control/monitor_client/plugins/linux/linux_cpu.py
@@ -2,4 +2,11 @@
 import subprocess
 
 def monitor():
-	pass
+	try:
+		shell_command = ''
+		result = subprocess.Popen(shell_command,shell=True,stdout=subprocess.PIPE).stdout.readlines()
+
+		value_dict = {'status': 0, 'data': {}}
+	except Exception as e:
+		
+
